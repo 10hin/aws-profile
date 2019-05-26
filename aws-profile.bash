5a6ae2df1c13d1fb8c62633a205c326eb4c5829b
10hin <10hin@outlook.jp>
2019-05-27 01:54:00 +0900

    fix bug in script environment initialization.

--- aws-profile.bash
+++ aws-profile.bash
@@ -1,23 +1,23 @@
 
-export __AWS_PROFILE_CREDENTIALS=~/.aws/credentials
-export __AWS_PROFILE_CONF_DIR=~/.aws-profile
-export __AWS_PROFILE_PROFILE_PERSISTENT_FILE="${__AWS_PROFILE_CONF_DIR}/current-profile"
-if ! ls "${__AWS_PROFILE_CONF_DIR}" &> /dev/null; then
-  mkdir -p "${__AWS_PROFILE_CONF_DIR}"
+export __AWS_PROFILE__CREDENTIALS=~/.aws/credentials
+export __AWS_PROFILE__CONF_DIR=~/.aws-profile
+export __AWS_PROFILE__PROFILE_PERSISTENT_FILE="${__AWS_PROFILE__CONF_DIR}/current-profile"
+if ! ls "${__AWS_PROFILE__CONF_DIR}" &> /dev/null; then
+  mkdir -p "${__AWS_PROFILE__CONF_DIR}"
 fi
-if ! [[ -f "${__AWS_PROFILE_PROFILE_PERSISTENT_FILE}" ]]; then
-  if [[ -d "${__AWS_PROFILE_PROFILE_CONF_DIR}" ]]; then
-    printf "default" > ${__AWS_PROFILE_PROFILE_PERSISTENT_FILE} 2> /dev/null
+if ! [[ -f "${__AWS_PROFILE__PROFILE_PERSISTENT_FILE}" ]]; then
+  if [[ -d "${__AWS_PROFILE__CONF_DIR}" ]]; then
+    printf "default" > ${__AWS_PROFILE__PROFILE_PERSISTENT_FILE} 2> /dev/null
   else
-    echo "aws-profile.bash: configuration directory name ${__AWS_PROFIE_CONF_DIR} exists, but not directory! cannot store configuration." 1>&2
+    echo "aws-profile.bash: configuration directory name ${__AWS_PROFILE__CONF_DIR} exists, but not directory! cannot store configuration." 1>&2
   fi
 fi
 
 function __aws_profile__list_profiles() {
   __aws_profile__check_and_reset_current_profile
-  grep -E '^\[[^][]*\]\s*$' "${__AWS_PROFILE_CREDENTIALS}" | sed -r -e 's/\[([^][]*)\]\s*$/\1/' |\
+  grep -E '^\[[^][]*\]\s*$' "${__AWS_PROFILE__CREDENTIALS}" | sed -r -e 's/\[([^][]*)\]\s*$/\1/' |\
   while read profile; do
-    if [[ "${profile}" == "$(cat ${__AWS_PROFILE_PROFILE_PERSISTENT_FILE})" ]]; then
+    if [[ "${profile}" == "$(cat ${__AWS_PROFILE__PROFILE_PERSISTENT_FILE})" ]]; then
       prefix="\x1b[33m* "
       suffix="\x1b[0m"
     else
@@ -45,11 +45,11 @@ function __aws_profile__show_help() {
          aws-profile <profile>
              set profile as <profile>
              <profile> must appear in profile credentials file.
-             (${__AWS_PROFILE_CREDENTIALS})
+             (${__AWS_PROFILE__CREDENTIALS})
 
          aws-profile ls
              list profile in cretentials file.
-             (${__AWS_PROFILE_CREDENTIALS})
+             (${__AWS_PROFILE__CREDENTIALS})
 
          aws-profile (-h|--help)
              show this help.
@@ -58,8 +58,8 @@ __EOT__
 }
 
 function __aws_profile__check_and_reset_current_profile() {
-  if ! cat "${__AWS_PROFILE_CREDENTIALS}" | grep -E "^\[$(cat ${__AWS_PROFILE_PROFILE_PERSISTENT_FILE})\]\s*$" &> /dev/null; then
-    if cat "${__AWS_PROFILE_CREDENTIALS}" | grep -E "^\[default\]\s*$" &> /dev/null; then
+  if ! cat "${__AWS_PROFILE__CREDENTIALS}" | grep -E "^\[$(cat ${__AWS_PROFILE__PROFILE_PERSISTENT_FILE})\]\s*$" &> /dev/null; then
+    if cat "${__AWS_PROFILE__CREDENTIALS}" | grep -E "^\[default\]\s*$" &> /dev/null; then
       __aws_profile__set_profile default
     else
       first_profile=$(grep -E '^\[[^][]*\]\s*$' | head -1 | sed -r -e 's/\[([^][]*)\]\s*$/\1/')
@@ -80,12 +80,12 @@ function __aws_profile__set_profile() {
     __aws_profile__show_help
     return 1
   fi
-  if ! cat ${__AWS_PROFILE_CREDENTIALS} | grep -E "^\[$1\]" &> /dev/null; then
+  if ! cat ${__AWS_PROFILE__CREDENTIALS} | grep -E "^\[$1\]" &> /dev/null; then
     echo "aws-profile: __aws_profile__set_profile: specified profile ($1) not exist in credentials file." 1>&2
     __aws_profile__show_help
     return 1
   fi
-  printf "$1" > "${__AWS_PROFILE_PROFILE_PERSISTENT_FILE}" 2> /dev/null
+  printf "$1" > "${__AWS_PROFILE__PROFILE_PERSISTENT_FILE}" 2> /dev/null
   export AWS_PROFILE="$1"
 }
 
@@ -110,4 +110,4 @@ function aws-profile() {
   esac
 }
 
-aws-profile "$(cat "${__AWS_PROFILE_PROFILE_PERSISTENT_FILE}")"
+aws-profile "$(cat "${__AWS_PROFILE__PROFILE_PERSISTENT_FILE}")"
